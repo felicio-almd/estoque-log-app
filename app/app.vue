bc38c88602f5055ad6eed89700a1816638b5228b
Felicio <feliciorar@gmail.com>
2025-10-09 23:06:58 -0300

    config lint, tailwind, themes, ui

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,18 @@
+<script setup lang="ts">
+console.log('oi')
+console.log('ashduhaus')
+console.log('ashduhaus')
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <UButton>CLIQUEEEEEEEEEEEEEEE</UButton>
+    <UButton color="primary">
+      My Brand Button
+    </UButton>
+    <h1>Teste de elenmento foassedaehjhu</h1>
+    <div>
+      <span>fashduhas</span>
+    </div>
+  </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
+  ui: {
+    //
+  },
+  colorMode: {
+    preference: 'system',
+    // preference: 'system',
+    fallback: 'dark',
+  },
 })
